fd1d00706e85dfcc5d8650074acfd758d20536e8
skendoh <skendoh@gmail.com>
2018-04-19 20:50:23 +0200

    First commit

---
+++ src/main/java/es/project/clashRoyale/ClashRoyaleApplication.java
@@ -0,0 +1,12 @@
+package es.project.clashRoyale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashRoyaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashRoyaleApplication.class, args);
+	}
+}
